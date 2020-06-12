0e46ab5f8798870e3bfcbe2c1254561a1a8e084f
zakaria <elattarz@yahoo.com>
2020-06-12 03:19:16 +0000

    added api specification, available on /swagger-ui.html

--- src/main/java/org/cannonbank/core/CoreApplication.java
+++ src/main/java/org/cannonbank/core/CoreApplication.java
@@ -2,8 +2,10 @@ package org.cannonbank.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+
 public class CoreApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/cannonbank/core/Security/security/WebSecurityConfig.java
+++ src/main/java/org/cannonbank/core/Security/security/WebSecurityConfig.java
@@ -54,12 +54,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+
+
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
-			.anyRequest().authenticated();
+				.antMatchers("/swagger-ui.html").permitAll()
+				.antMatchers("/swagger-ui.html/**").permitAll()
+				.antMatchers("/swagger-resources").permitAll()
+				.antMatchers("/swagger-resources/**").permitAll()
+				.antMatchers("/webjars/**").permitAll()
+				.antMatchers("/v2/api-docs").permitAll()
+				.antMatchers("/v2/api-docs/**").permitAll()
+
+				.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
